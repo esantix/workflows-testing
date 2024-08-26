23027b13f16ef915ed3dd8345a8318031ec5153d
esantix <santiago93echevarria@gmail.com>
2024-08-26 13:50:52 -0300

    Updates

---
+++ python/src/test/test_mock.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class MockTest(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def test_1(self):
+        assert True
